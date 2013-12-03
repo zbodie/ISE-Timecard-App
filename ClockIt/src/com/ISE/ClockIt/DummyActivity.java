f319475e7356a277c3774929c89e7ee85a3fcff6
zbodie <zach.bodensteiner@gmail.com>
2013-12-03 17:49:02 -0600

    Dummy Activity

---
+++ ClockIt/src/com/ISE/ClockIt/DummyActivity.java
@@ -0,0 +1,18 @@
+package com.ISE.ClockIt;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ISE Developer
+ * Date: 12/3/13
+ * Time: 5:13 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class DummyActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dummy);
+    }
+}
--- ClockIt/src/com/ISE/ClockIt/MyActivity.java
+++ ClockIt/src/com/ISE/ClockIt/MyActivity.java
@@ -1,7 +1,10 @@
 package com.ISE.ClockIt;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MyActivity extends Activity {
     /**
@@ -11,5 +14,18 @@ public class MyActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        final Button signIn;
+        signIn = (Button)findViewById(R.id.button);
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startDummyActivity();
+            }
+        });
+
+    }
+    public void startDummyActivity(){
+        Intent dummyIntent = new Intent(this, DummyActivity.class);
+        startActivity(dummyIntent);
     }
 }
